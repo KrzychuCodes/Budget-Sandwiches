5b7240e8aac2e26f240a7b679cc2e91375622c74
Krzychu_codes <k.badelek@gmail.com>
2024-03-01 14:01:46 +0100

    Add project files.

---
+++ Budget Sandwiches/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Budget_Sandwiches
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Budget Sandwiches/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_Sandwiches
+{
+     class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "spices", "french sauce" };
+        public string[] Breads = {"rice", "white", "wheat", "pumpernickel", "bun" };
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = randomProtein + " i " + randomCondiment + ". Pieczywo: " + randomBread + ".";
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+
+
+}
